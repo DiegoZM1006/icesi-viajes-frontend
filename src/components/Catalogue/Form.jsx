84bad036a0519ce6294e00e4ab4db192c57dc9b1
DiegoZM1006 <1109184718@u.icesi.edu.co>
2024-05-14 23:57:23 -0500

    feat: Add service to save a destination

--- src/components/Catalogue/Form.jsx
+++ src/components/Catalogue/Form.jsx
@@ -1,26 +1,82 @@
 import { Link } from "react-router-dom";
 import PropTypes from 'prop-types';
+import { useState } from "react";
+import { addDestination } from "../../services/addDestination";
 
 function Form(props) {
+
+    const [valid, setValid] = useState();
+    const [errors, setErrors] = useState();
+    const [imagePreview, setImagePreview] = useState(null);
+
+    const handleFileChange = (event) => {
+        event.preventDefault();
+
+        const file = event.target.files[0];
+        const reader = new FileReader();
+
+        reader.onload = () => {
+            setImagePreview(reader.result);
+        };
+
+        reader.readAsDataURL(file);
+    };
+
+    const handleAddDestination = async (event) => {
+        event.preventDefault()
+    
+        const description = event.target.description.value
+        const information = event.target.more_info.value
+        const country = event.target.country.value
+        const city = event.target.city.value
+        const days = event.target.days.value
+        const nights = event.target.nights.value
+        const price = event.target.price.value
+        const name = event.target.name.value
+        const hotel = event.target.hotel.value
+        const tickets = event.target.tickets.value
+        const image = event.target.input_file.files[0]
+    
+        const data = {
+            name,
+            description,
+            information,
+            country,
+            city,
+            days,
+            nights,
+            price,
+            hotel,
+            tickets,
+            image
+        }
+    
+        await addDestination(data, setErrors, setValid)
+    }
+
+    
+
     return (
-        <form className="[box-shadow:rgba(0,_0,_0,_0.16)_0px_3px_6px,_rgba(0,_0,_0,_0.23)_0px_3px_6px] h-full bg-[--var-light] flex gap-6 p-5 rounded-lg" action="">
+        <form className="[box-shadow:rgba(0,_0,_0,_0.16)_0px_3px_6px,_rgba(0,_0,_0,_0.23)_0px_3px_6px] h-full bg-[--var-light] flex gap-6 p-5 rounded-lg" onSubmit={handleAddDestination}>
             <section className="flex flex-col gap-3">
-                <label htmlFor="input-file" id="drop-area" className="flex-grow">
-                    <input type="file" id="input-file" name="input-file" accept=".png" hidden />
-                        <div className="img-view w-[365px] h-full min-h-[365px] border-2 border-black border-dashed rounded-2xl flex justify-center items-center flex-col gap-3 bg-gray-100 transition-all duration-200 ease-linear"> 
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-12 h-12 text-blue-600" viewBox="0 0 24 24">
-                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6zM9.998 14.768H8.895v3.274h-.917v-3.274H6.893V14h3.105v.768zm2.725 3.274-.365-.731c-.15-.282-.246-.492-.359-.726h-.013c-.083.233-.185.443-.312.726l-.335.731h-1.045l1.171-2.045L10.336 14h1.05l.354.738c.121.245.21.443.306.671h.013c.096-.258.174-.438.276-.671l.341-.738h1.043l-1.139 1.973 1.198 2.069h-1.055zm4.384-3.274h-1.104v3.274h-.917v-3.274h-1.085V14h3.105v.768zM14 9h-1V4l5 5h-4z"></path>
-                        </svg>
-                        <p>Arrastra una imagen</p>
+                <label htmlFor="input_file" id="drop-area" className="flex-grow">
+                    <input type="file" id="input_file" name="input_file" accept=".jpg" hidden onChange={handleFileChange} />
+                    <div className="img-view w-[365px] h-full min-h-[365px] border-2 border-black border-dashed rounded-2xl flex justify-center items-center flex-col gap-3 bg-gray-100 transition-all duration-200 ease-linear bg-cover" style={{ backgroundImage: `url(${imagePreview})` }}>
+                        {
+                            imagePreview ? <p className="text-white bg-black bg-opacity-50 p-2 rounded-md">Imagen cargada</p> : <p className="text-black">Arrastra una imagen o haz click para seleccionar</p>
+                        }
                     </div>
                 </label>
+                <button type="button" className="px-3 py-2 bg-[--var-dark] text-white rounded-md" onClick={() => setImagePreview(null)}>
+                    Borrar imagen
+                </button>
                 <div>
                     <label htmlFor="description">Descripción</label>
                     <textarea value={props.description} type="text" id='description' name='description' placeholder='Escribe una descripción del destino' className='w-full mt-1 p-2 border border-gray-300 rounded-md' />
                 </div> 
                 <div>
-                    <label htmlFor="more-info">+ Informacion</label>
-                    <textarea value={props.information} type="text" id='more-info' name='more-info' placeholder='Escribe informacion adicional de lo que tenga el destino' className='w-full mt-1 p-2 border border-gray-300 rounded-md' />
+                    <label htmlFor="more_info">+ Informacion</label>
+                    <textarea value={props.information} type="text" id='more_info' name='more_info' placeholder='Escribe informacion adicional de lo que tenga el destino' className='w-full mt-1 p-2 border border-gray-300 rounded-md' />
                 </div> 
             </section>
             <section className="flex flex-col gap-2 justify-center">
@@ -47,7 +103,7 @@ function Form(props) {
                     </div>
                 </div>
                 <div className='flex flex-row gap-2'>
-                    <div>
+                    <div className='flex-1'>
                         <label htmlFor="tickets">Tickets de avion</label>
                         <select name="tickets" id="tickets" className="w-full mt-1 p-2 border border-gray-300 rounded-md">
                             <option value="ida-vuelta">Ida y Vuelta</option>
@@ -55,7 +111,7 @@ function Form(props) {
                             <option value="none">No incluye</option>
                         </select>
                     </div>
-                    <div>
+                    <div className='flex-1'>
                         <label htmlFor="price">Precio</label>
                         <input value={props.price} type="number" id='price' name='price' placeholder='Escribe el precio x persona' className='w-full mt-1 p-2 border border-gray-300 rounded-md' />
                     </div>
@@ -74,6 +130,8 @@ function Form(props) {
                     </Link>
                     <button type="submit" className="flex-grow px-3 py-2 bg-[--var-success] text-white rounded-md">Crear</button>
                 </div>
+                {errors && <p className='text-red-600'>{errors}</p>}
+                {valid && <p className='text-green-600'>{valid}</p>}
             </section>
         </form>
     );
